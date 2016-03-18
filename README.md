a4e1bb821a17e3de79e997f3d84f32353957c3bb
danilolekovic <danilo@lekovic.ca>
2016-03-18 11:15:24 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # haar-negative
-Python script for getting negative samples quickly and efficiently for HAAR cascade training.
+
+Small Python script for getting negative samples quickly and efficiently for HAAR cascade training.
+
+## how
+
+```
+$ git clone https://github.com/danilolekovic/haar-negative.git
+$ cd haar-negative
+$ python src/main.py
+```
+
+## HAAR Cascade Training
+
+[@sherrardTr4129](https://github.com/sherrardTr4129) has written a very good tutorial [right here](http://www.trevorsherrard.com/Haar_training.html). 
---
+++ src/main.py
@@ -0,0 +1,53 @@
+import urllib, sys, httplib, os
+from urlparse import urlparse
+
+MAX = 4875
+
+def checkUrl(url):
+    p = urlparse(url)
+    conn = httplib.HTTPConnection(p.netloc)
+    conn.request('HEAD', p.path)
+    resp = conn.getresponse()
+    return resp.status < 400
+
+def makeLink(i):
+    string = str(i)
+    num = "0002"
+
+    if len(string) <= 1:
+        num = "000" + str(i)
+    elif len(string) == 2:
+        num = "00" + str(i)
+    elif len(string) == 3:
+        num = "0" + str(i)
+    elif len(string) == 4:
+        num = str(i)
+
+    return "http://tutorial-haartraining.googlecode.com/svn/trunk/data/negatives/neg-" + num + ".jpg"
+
+def getImages(i):
+    if i > MAX:
+        i = MAX
+
+    i = i + 2
+    num = 2
+
+    if not os.path.exists("negative_images/"):
+        os.makedirs("negative_images/")
+
+    while num < i:
+        theURL = makeLink(num)
+
+        if checkUrl(theURL) is True:
+            perc = 100 * float(num) / float(i)
+            sys.stdout.write("Download progress: %d%%   \r" % (perc) )
+            sys.stdout.flush()
+            urllib.urlretrieve(theURL, "negative_images/neg-" + str(num) + ".jpg")
+            num += 1
+        else:
+            num += 1
+            i += 1
+
+    print "Finished: " + str(num) + " images downloaded."
+
+getImages(600)
